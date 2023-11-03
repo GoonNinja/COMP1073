27ce87c484bf0406a733bf6b669b6efad707f41d
Adam Miszkiewicz <96786287+GoonNinja@users.noreply.github.com>
2023-11-03 10:55:09 -0400

    Add files via upload

---
+++ JS Lab 8/js/index.js
@@ -0,0 +1,28 @@
+var output = document.getElementById('output');
+
+//Create a hamburger object
+class Hamburger {
+    constructor(bun, veggie, cheese, sauce, meatType, pattyAmount, sides) {
+        this.bun = bun;
+        this.veggie = veggie;
+        this.cheese = cheese;
+        this.sauce = sauce;
+        this.meatType = meatType;
+        this.pattyAmount = pattyAmount;
+        this.sides = sides;
+        this.bio = function () {
+            output.innerHTML = "Bun: " + this.bun + '<br>' +
+                "Veggie: " + this.veggie + '<br>' +
+                "Cheese: " + this.cheese + '<br>' +
+                "Sauce: " + this.sauce + '<br>' +
+                "Meat Type: " + this.meatType + '<br>' +
+                "Patty Amount: " + this.pattyAmount + '<br>' +
+                "Sides: " + this.sides;
+        };
+    }
+}
+
+//Create a hamburger with specific options
+var burger1 = new Hamburger('Sesame Seed',['Lettuce', 'Tomato', 'Red Onion'], 'Swiss', 'Ketchup', 'Beef', 'Double', 'Fries');
+
+burger1.bio();
